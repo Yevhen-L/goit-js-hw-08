26c479c4d2f063fe14f834c1083f90f72aa1cc1b
Yevhen-L <ujeen2101@gmail.com>
2023-08-05 14:31:21 +0300

    add task2 is correct

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const playerElement = document.getElementById('vimeo-player');
+
+const player = new VimeoPlayer(playerElement);
+
+const saveCurrentTime = throttle((time) => {
+  localStorage.setItem('videoplayer-current-time', time);
+}, 1000); 
+
+player.on('timeupdate', (data) => {
+  const currentTime = data.seconds;
+  saveCurrentTime(currentTime);
+});
+
+function restorePlaybackTime() {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(parseFloat(savedTime));
+  }
+}
+player.ready().then(() => {
+  restorePlaybackTime();
+});
