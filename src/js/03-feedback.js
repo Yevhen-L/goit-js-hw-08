fff085f6d4bc28c8a98396e9f1567a0add70c306
Yevhen-L <ujeen2101@gmail.com>
2023-08-05 14:45:23 +0300

    add task3 is correct

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
 
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');
+const saveFormState = throttle(() => {
+const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}, 500);
+
+emailInput.addEventListener('input', saveFormState);
+messageInput.addEventListener('input', saveFormState);
+
+function restoreFormState() {
+  const savedState = localStorage.getItem('feedback-form-state');
+  if (savedState) {
+    const formState = JSON.parse(savedState);
+    emailInput.value = formState.email;
+    messageInput.value = formState.message;
+  }
+}
+document.addEventListener('DOMContentLoaded', restoreFormState);
+
+feedbackForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  emailInput.value = '';
+  messageInput.value = '';
+  localStorage.removeItem('feedback-form-state');
+  console.log('Form submitted:', formState);
+});
